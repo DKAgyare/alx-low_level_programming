d5c46d1f8667c9f8f1beefde5eb1920c2641852c
DKAgyare <DKAgyare@hotmail.com>
2022-12-20 05:49:02 -0800

    prints half of string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * puts_half - prints half of a string
+ * @str: input
+ */
+
+void puts_half(char *str)
+{
+	int index
+	int half;
+
+	index = 0;
+	while (str[index] != '\0')
+		index++;
+	haf = index / 2;
+
+	if (index % 2 == 1)
+		half++;
+
+	while (half < index)
+	{
+		putchar(str[half]);
+		half++;
+	}
+	putchar('\n');
+}
