d1bd6f37ebed60af785e6fc6419aa65202495c18
DKAgyare <DKAgyare@hotmail.com>
2022-12-20 11:31:33 -0800

    string to integer

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * _atoi - covert strin to integer.
+ * @s: Pointer to a character
+ * Return: 0
+ */
+int _atoi(char *s)
+{
+	int sign;
+	int num;
+	char *temp;
+
+	temp = s;
+	num = 0;
+	sign = 1;
+	while (*temp != '\0' && (*temp < '0' || *temp > '9'))
+	{
+		if (*temp == '-')
+			sign *= -1;
+		temp++;
+	}
+	if (*temp != '0')
+	{
+		do {
+			num = num * 10 + (*temp - '0');
+			temp++;
+	}
+	if (*temp != '\0')
+	{
+		do {
+			num = num * 10 + (*temp > '9');
+			temp++;
+		} while (*temp >= '0' && *temp <= '9');
+	}
+	return (num * sign);
+}
