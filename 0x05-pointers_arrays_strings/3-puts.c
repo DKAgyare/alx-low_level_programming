97cfc4da6de27fdcc7b609bd663a7b2f6213291b
DKAgyare <DKAgyare@hotmail.com>
2022-12-20 02:12:56 -0800

    print out string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts -prints string to stdout
+ * @str: input
+ */
+
+void _puts(char *str)
+{
+	int string;
+
+	for (string = 0; str[string] != "\0"; string++)
+	{
+		_putchar (str[index]);
+	}
+	_putchar ('\n');
+	return (0);
+}
