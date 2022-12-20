45baf2c2b85ea0ab17bcddb5d4e635301818f823
DKAgyare <DKAgyare@hotmail.com>
2022-12-20 02:49:44 -0800

    print out string

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * _puts -prints string to stdout
- * @str: input
+ * _puts - prints string to stdout
+ * @str: string to print
  */
 
 void _puts(char *str)
@@ -12,5 +12,4 @@ void _puts(char *str)
 		_putchar(*str++);
 	}
 	_putchar('\n');
-	
 }
