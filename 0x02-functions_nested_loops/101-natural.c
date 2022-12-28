bfe9e5d40c3f7de148471d41e153e50d93dcdc28
DKAgyare <dkagyare@hotmail.com>
2022-12-28 06:22:55 -0800

    13. natural numbers

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,12 +1,13 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * main - check the code
  *
  * Return: Always 0
  */
-int main(void)
+void print_times_table(int n);
+
+int main()
 {
 	print_times_table(3);
 	_putchar('\n');
