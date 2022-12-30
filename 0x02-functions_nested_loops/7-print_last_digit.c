ca2ed9a4bb718345bd06f5c5d970d9087f3eae63
DKAgyare <dkagyare@hotmail.com>
2022-12-30 02:57:20 -0800

    print last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -6,12 +6,15 @@
  */
 int print_last_digit(int n)
 {
-	int last = n % 10;
+	int last;
+
+	last = n % 10;
 
 	if (last < 0)
-		last *= -1;
-
+	{
+		last = last * -1;
+	}
 	_putchar(last + '0');
 
-	return (0);
+	return (last);
 }
